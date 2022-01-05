88551c5b317e26072ca442781c0d904d592bdbf7
Aurora <tomek-kubel2@wp.pl>
2022-01-05 17:54:00 +0100

    remove play mode + update audio-capture

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use std::{
     collections::VecDeque,
-    fs::File,
-    io::{self, BufReader},
-    path::{Path, PathBuf},
     sync::Arc,
     time::Duration,
 };
@@ -25,14 +22,6 @@ use buttplug::{
 };
 use clap::Parser;
 use parking_lot::Mutex;
-use rodio::{Decoder, OutputStream, Sink, Source};
-
-fn open_decoder(
-    path: impl AsRef<Path>,
-) -> io::Result<Decoder<BufReader<File>>> {
-    Decoder::new(BufReader::new(File::open(path)?))
-        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
-}
 
 async fn start_bp_server() -> Result<ButtplugClient, ButtplugClientError> {
     let remote_connector = RemoteConn::<_, JsonSer>::new(
@@ -81,15 +70,12 @@ fn get_device(
 
 #[derive(Parser)]
 enum Opt {
-    /// [WIP] Plays music file
-    Play(Play),
     /// Listens to system audio
     Listen(Listen),
 }
 
 fn main() {
     match Opt::parse() {
-        Opt::Play(args) => play(args),
         Opt::Listen(args) => listen(args),
     }
 }
@@ -194,70 +180,6 @@ fn calculate_power(samples: &[f32], channels: usize) -> Vec<f32> {
 fn avg(samples: &[f32]) -> f32 {
     let len = samples.len();
     samples.iter().sum::<f32>() / len as f32
-}
-
-#[derive(Parser)]
-struct Play {
-    #[clap(short, long, default_value = "64")]
-    chunk_size: usize,
-    /// Path to audio file
-    file: PathBuf,
-}
-
-fn play(args: Play) {
-    let chunk_size = args.chunk_size;
-
-    // Start audio
-    let (_stream, handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&handle).unwrap();
-    sink.pause();
-
-    // Prepare audio
-    let file_name = args.file;
-    let audio = open_decoder(file_name).unwrap().buffered();
-    // .take_duration(Duration::from_secs(30));
-    let (tx, rx) = flume::bounded(0);
-    let sample_rate = audio.sample_rate();
-    let channels = audio.channels() as usize;
-    let dur = Duration::from_secs(1) * chunk_size as u32 / sample_rate;
-    eprintln!("dur: {:?}", dur);
-    let audio2 = audio.clone().convert_samples::<f32>();
-    let audio = audio.periodic_access(dur, move |_| {
-        let _ = tx.send(());
-    });
-    sink.append(audio);
-
-    block_on(async {
-        let client = start_bp_server().await?;
-
-        // TODO: handle more than 1 device
-        let device = get_device(&client).unwrap();
-        eprintln!("found device: {}", device.name);
-
-        let vib_count = device
-            .allowed_messages
-            .get(&MsgType::VibrateCmd)
-            .and_then(|x| x.feature_count)
-            .expect("no vibrators");
-        eprintln!("vibrators: {}", vib_count);
-
-        sink.play();
-        for chunk in audio2.chunks(chunk_size * channels) {
-            let speeds = calculate_power(&chunk, channels);
-            let avg = avg(&speeds);
-            let speeds = vec![avg as _; vib_count as usize];
-            device.vibrate(VibrateCommand::SpeedVec(speeds)).await?;
-
-            rx.recv().unwrap();
-        }
-        drop(rx);
-        client.stop_all_devices().await?;
-        client.disconnect().await?;
-        eprintln!("buttplug finished");
-        Ok::<_, ButtplugClientError>(())
-    })
-    .unwrap();
-    sink.sleep_until_end();
 }
 
 trait IterChunksExt: Iterator + Sized {
